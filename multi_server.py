b57cf498f9fdbd1e59f25b0aaad0067598209a24
OllyOxenFree <44375265+OllyOxenFree@users.noreply.github.com>
2022-05-21 20:22:42 -0400

    Update multi_server.py

--- multi_server.py
+++ multi_server.py
@@ -1,47 +1,64 @@
 import socket
-import os
-import sys
 import time
+import sys
+import queue
+import select
 
-HOST = "192.168.1.11"
-PORT = 8590
+HOST = "192.168.1.8"
+PORT = 7290
 BUFFER = 1024
-s = socket.socket()
-s.bind((HOST,PORT))
-s.listen(10)
+main_sock = socket.socket()
+print("Socket created")
+main_sock.bind((HOST,PORT))
+print("Socket binded")
+main_sock.listen(5)
+print("Waiting for connection...")
 
-childrenInAction = []
-def reapChildren():
-    while childrenInAction:
-        pid, stat = os.waitpid(0, os.WNOHANG)
-        if not pid:
-            break
-        childrenInAction.remove(pid)
+readList = [main_sock]
+writeList = []
+errorList = []
+msg_queue = {}
 
-#def transmitMessage(message)
 
-def handleClient(connection):
-    time.sleep(10)
-    while True:
-        message = connection.recv(BUFFER)
-        if(not message):
-            break
-        broadcast = "Echo=>%s at %s" % (message, time.ctime(time.time()))
-        connection.send(broadcast.encode())
-    connection.close()
-    os._exit(0)
-
-def transmitter():
-    while True:
-        print("Server is actively listening for clients")
-        client, address = s.accept()
-        print("Server connection established at", address, "at", time.ctime(time.time()))
-        reapChildren()
-        childPid = os.fork()
-        if childPid == 0:
-            handleClient(connection)
+while 1:
+    reads, writes, errors = select.select(readList, writeList, errorList)
+    for sock in reads:
+        if sock == main_sock:
+            connection, addr = sock.accept()
+            connection.setblocking(1)
+            print("New client from %s:%d" %addr)
+            connection.setblocking(0)
+            readList.append(connection)
+            msg_queue[connection] = queue.Queue()
         else:
-            childrenInAction.append(childPid)
+            data = sock.recv(BUFFER)
+            if data:
+                print("Received data")
+                msg_queue[sock].put(data)
+                if sock not in writeList:
+                    writeList.append(sock)
+            else:
+                if sock in writeList:
+                    writeList.remove(sock)
+                readList.remove(sock)
+                sock.close()
+                del msg_queue[sock]
 
-transmitter()
-s.close()
+    for sock in writes:
+        try:
+            next_msg = msg_queue[sock].get_nowait()
+        except queue.Empty:
+            writeList.remove(sock)
+        else:
+            sock.send(next_msg.encode())
+
+    for sock in errors:
+        if sock in readList:
+            readList.remove(sock)
+        if sock in writeList:
+            writeList.remove(sock)
+        if sock in errorList:
+            errorList.remove(sock)
+        sock.close()
+
+        del msg_queue[sock]
