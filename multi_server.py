ab20cf85cf363d1f6e0ddc57d17729c135c079e3
OllyOxenFree <44375265+OllyOxenFree@users.noreply.github.com>
2022-05-22 13:03:02 -0400

    Update multi_server.py

--- multi_server.py
+++ multi_server.py
@@ -4,7 +4,7 @@ import sys
 import queue
 import select
 
-HOST = "192.168.1.8"
+HOST = "192.168.1.4"
 PORT = 7290
 BUFFER = 1024
 main_sock = socket.socket()
@@ -18,8 +18,20 @@ readList = [main_sock]
 writeList = []
 errorList = []
 msg_queue = {}
-
-
+"""
+def broadcast(sock, msg):
+    for s in readList:
+        if s != main_sock and s != sock:
+            try:
+                s.send(msg)
+            except:
+                s.close()
+                readList.remove(s)
+                if s in writeList:
+                    writeList.remove(s)
+            else:
+                print("Broadcast successful")
+"""
 while 1:
     reads, writes, errors = select.select(readList, writeList, errorList)
     for sock in reads:
@@ -30,27 +42,36 @@ while 1:
             readList.append(connection)
             msg_queue[connection] = queue.Queue()
         else:
-            data = sock.recv(BUFFER)
-            if data:
-                print("Received data")
-                msg_queue[sock].put(data)
-                if sock not in writeList:
-                    writeList.append(sock)
+            try:
+                data = sock.recv(BUFFER)
+            except:
+                print("Connection from", addr, "has been lost.")
+                readList.remove(sock)
+                if sock in writeList:
+                    writeList.remove(sock)
+                    sock.close()
+                    del msg_queue[sock]
+            else:
+                if data:
+                    print("Received data from", addr, ":", data)
+                    msg_queue[sock].put(data)
+                    if sock not in writeList:
+                        writeList.append(sock)
+            """
             else:
                 if sock in writeList:
                     writeList.remove(sock)
                 readList.remove(sock)
                 sock.close()
                 del msg_queue[sock]
-
+            """
     for sock in writes:
         try:
             next_msg = msg_queue[sock].get_nowait()
         except queue.Empty:
             writeList.remove(sock)
         else:
-            sock.send(next_msg.encode())
-
+            sock.send(next_msg)
     for sock in errors:
         if sock in readList:
             readList.remove(sock)
